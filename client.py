44e756f05df35c6c911d07b1c7f494ca261f67c8
Vasile Groza <Vasile.Groza@tss-yonder.com>
2018-05-20 01:57:16 +0300

    Add delay metric between server-client

--- client.py
+++ client.py
@@ -1,31 +1,77 @@
 import socket
-from random import choice
-from string import ascii_uppercase
-import time 
+import random 
+from struct import pack, unpack
+import time
+import datetime
+import argparse
 
-HOST, PORT = "localhost", 9999
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("client_type", type=str,
+                    help="please provide client type  normal/proxy")
+parser.add_argument("endpoint",
+                    help="please input the target hostname so we know where to send tcp packag")
+
+args = parser.parse_args()
+
+HOST, PORT = args.endpoint, 9999
 
 BUFFER_SIZE = 1214
-# to do 
-# 1. make client to start with args 
-#   - client_type ex proxy --> (s2, s3)[have to listhen to the tcp connection too], normal s1.
-#   - endpoint (aka hostname to the next pont for tcp package to get)
-#   - analysed_metric one of [delay, rtt]
+# to do
+# 1. make client to start with args
+#   + client_type ex proxy --> (s2, s3)[have to listhen to the tcp connection too], normal s1.
+#   + endpoint (aka hostname to the next pont for tcp package to get)
 # 2. calculate delay and rtt
 #   - find a elegant way to calculate rtt and store it in sql
 #   - can get timestamp store it in package and forward the pachage for delay
-#   - time_send: C -->S: time_receive S---> C time_ack  rtt = time_ack-time_send; store it to the sql 
-def client_tcp():
+#   - time_send: C -->S: time_receive S---> C time_ack  rtt = time_ack-time_send; store it to the sql
 
+# package format  
+def do_delay_metric():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 32)
-
+    now = time.time()
+    date = datetime.datetime.fromtimestamp(now).strftime("%Y-%m-%d %H:%M:%S.%f")
+    print(date)
+    package = pack('c26s',b'd',date.encode())
+    print(package)
     try:
-
         sock.connect((HOST, PORT))
-        print(sock.send(str.encode(''.join(choice(ascii_uppercase) for i in range(1214)))))
+        sock.send(package)
+    finally:
+        print('close socket')
+        sock.close()
 
+def do_rtt_metric():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 32)
+    try:
+        sock.connect((HOST, PORT))
+        # print(sock.send(str.encode(''.join(random.choice(ascii_uppercase)
+                                        #    for i in range(1214)))))
     finally:
         sock.close()
 
-client_tcp()
+def do_metrics():
+    """
+    This function will call two functions one for delay metric
+    and other for rtt metric.
+    """
+    do_delay_metric()
+    # do_rtt_metric()
+    
+
+def main():
+    while 1:
+        do_metrics()
+        sleep_time = random.randint(5,10)
+        print('sleep', sleep_time)
+        time.sleep(sleep_time)
+
+    pass
+if __name__=="__main__":
+    """
+    This is the entrypoint of the current script
+    """
+    main()
--- server.py
+++ server.py
@@ -1,19 +1,33 @@
 import socketserver
+from struct import pack, unpack
+import time
+import datetime
+
 BUFFER_SIZE = 1214
 # to do 
 # 1. make server to listen at the port 80
 #   - receive package and store delays to the db;
+#   - unpack it and get timestam date = datetime.datetime.fromtimestamp(your_timestamp)
+#   - first of all have to know which requesti is which unpack('c',package[:1])
 class TCPHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
 
         print("Connected address: {}".format(self.client_address[0]))
         data = self.request.recv(BUFFER_SIZE)
-        print('received from client', data)
+        if(data[:1]==b'd'):
+            self.handle_delay_request(data)
 
     def finish(self):
         print('Done')
         return socketserver.BaseRequestHandler.finish(self)
+    def handle_delay_request(self, data):
+        char, date = unpack('c26s', data)
+        time0 = datetime.datetime.strptime(date.decode(),"%Y-%m-%d %H:%M:%S.%f")
+        time0 = time.mktime(time0.timetuple())
+        time1 = time.time()
+        delay = time1-time0
+        print('delay----->', delay)
 
 def run_tcp_server(HOST, PORT):
 
